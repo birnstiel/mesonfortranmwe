4bd49d2e06e0600fd72a985191c77974173a30e5
Til Birnstiel <birnstiel@me.com>
2024-07-23 20:39:36 +0200

    initial commit

---
+++ mwe/fortran.f90
@@ -0,0 +1,12 @@
+module fortran
+    implicit none
+    
+contains
+
+subroutine printing()
+    implicit none
+    write(*,*) 'Printing from Fortran'
+    
+end subroutine printing
+    
+end module fortran
